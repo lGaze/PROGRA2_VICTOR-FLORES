851d7a00d9b3f20c4b65b018ae369920bd7a9d9a
Victor Hugo Flores <idv16c.vflores@uartesdigitles.edu.mx>
2018-01-20 01:10:22 -0600

    ADD modelos

---
+++ Parser.x/Parser_x/Parser_x.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <stdio.h>
+
+
+class Parseador
+{
+public:
+	Parseador();
+	~Parseador();
+   	void Abrir();
+
+private:
+
+};
+
+struct vertex
+{
+	double x, y, z;
+
+};
+
+
+
---
+++ Parser.x/TestApp/TestApp.cpp
@@ -0,0 +1,16 @@
+// TestApp.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include <Parser_x.h>
+
+
+int main()
+{
+	Parseador parseador;
+	parseador.Abrir();
+
+    return 0;
+
+}
+
